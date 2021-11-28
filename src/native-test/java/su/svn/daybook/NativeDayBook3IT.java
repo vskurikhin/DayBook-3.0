5502d9f8787d39a471326a11355bbfb500292064
Victor N. Skurikhin <vs@lnhost.net>
2021-11-28 12:23:44 +0300

    #1 Building Quarkus apps with Gradle

---
+++ src/native-test/java/su/svn/daybook/NativeDayBook3IT.java
@@ -0,0 +1,9 @@
+package su.svn.daybook;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeDayBook3IT extends DayBook3Test {
+
+    // Execute the same tests but in native mode.
+}
