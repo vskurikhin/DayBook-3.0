239792b34328fe8f7dea6b9f64c2f666162dcac9
Victor N. Skurikhin <vs@lnhost.net>
2021-12-06 18:25:13 +0300

    #11 Embed Liquibase into your product.

---
+++ src/main/resources/db/changelog/db/alter_user.sql
@@ -0,0 +1,11 @@
+/*
+ * This file was last modified at 2021.12.06 18:25 by Victor N. Skurikhin.
+ * This is free and unencumbered software released into the public domain.
+ * For more information, please refer to <http://unlicense.org>
+ * alter_user.sql
+ * $Id$
+ */
+
+GRANT ALL PRIVILEGES ON DATABASE dbuser TO db;
+ALTER USER dbuser PASSWORD 'password';
+ALTER USER dbuser SUPERUSER;
