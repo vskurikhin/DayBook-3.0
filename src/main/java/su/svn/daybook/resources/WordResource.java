38b27e968b926e7326abeb1c062b5265f4e43210
Скурихин Виктор Николаевич <VNSkurikhin@sberbank.ru>
2022-03-24 13:27:40 +0300

    #52 Refactoring liquibase migration scripts.

--- src/main/java/su/svn/daybook/resources/WordResource.java
+++ src/main/java/su/svn/daybook/resources/WordResource.java
@@ -1,5 +1,5 @@
 /*
- * This file was last modified at 2022.03.24 13:26 by Victor N. Skurikhin.
+ * This file was last modified at 2022.03.24 13:27 by Victor N. Skurikhin.
  * This is free and unencumbered software released into the public domain.
  * For more information, please refer to <http://unlicense.org>
  * WordResource.java
--- src/main/resources/db/changelog/db/Change-Sets/115_date_2021_01_14_time_17_15_11_issues_52_public_dual_postgresql.sql
+++ src/main/resources/db/changelog/db/Change-Sets/115_date_2021_01_14_time_17_15_11_issues_52_public_dual_postgresql.sql
@@ -1,5 +1,5 @@
 /*
- * This file was last modified at 2022.03.24 13:26 by Victor N. Skurikhin.
+ * This file was last modified at 2022.03.24 13:27 by Victor N. Skurikhin.
  * This is free and unencumbered software released into the public domain.
  * For more information, please refer to <http://unlicense.org>
  * 115_date_2021_01_14_time_17_15_11_issues_52_public_dual_postgresql.sql
